11e2cbf2cb09833bca7668085a3138fbd903691f
Ben <b.johnson9356@gmail.com>
2026-07-12 11:17:14 +0100

    fix: support Laravel 12 marketplace callbacks

--- scripts/run-parallel-pest.php
+++ scripts/run-parallel-pest.php
@@ -5,13 +5,9 @@ declare(strict_types=1);
 $processes = getenv('PEST_PROCESSES') ?: '4';
 $shard = getenv('PEST_SHARD') ?: '1/1';
 
-if (preg_match('/^[1-9][0-9]*$/', $processes) !== 1) {
-    throw new InvalidArgumentException('PEST_PROCESSES must be a positive integer.');
-}
+throw_if(preg_match('/^[1-9]\d*$/', $processes) !== 1, InvalidArgumentException::class, 'PEST_PROCESSES must be a positive integer.');
 
-if (preg_match('/^[1-9][0-9]*\/[1-9][0-9]*$/', $shard) !== 1) {
-    throw new InvalidArgumentException('PEST_SHARD must use the N/TOTAL format.');
-}
+throw_if(preg_match('/^[1-9]\d*\/[1-9]\d*$/', $shard) !== 1, InvalidArgumentException::class, 'PEST_SHARD must use the N/TOTAL format.');
 
 $arguments = array_map(
     static fn (string $argument): string => str_replace(
@@ -30,6 +26,4 @@ $command = array_merge(
 $escapedCommand = implode(' ', array_map(escapeshellarg(...), $command));
 passthru($escapedCommand, $exitCode);
 
-if ($exitCode !== 0) {
-    throw new RuntimeException("Pest failed with exit code {$exitCode}.", $exitCode);
-}
+throw_if($exitCode !== 0, RuntimeException::class, sprintf('Pest failed with exit code %d.', $exitCode), $exitCode);
