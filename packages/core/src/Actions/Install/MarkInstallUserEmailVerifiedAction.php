733639e49c9fb85958285295ad43667fa2147b2e
Ben <howdu@users.noreply.github.com>
2026-07-19 12:00:00 +0100

    fix: satisfy granular action trait contract

--- packages/core/src/Actions/Install/MarkInstallUserEmailVerifiedAction.php
+++ packages/core/src/Actions/Install/MarkInstallUserEmailVerifiedAction.php
@@ -6,10 +6,12 @@ namespace Capell\Core\Actions\Install;
 
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Lorisleiva\Actions\Concerns\AsFake;
 use Lorisleiva\Actions\Concerns\AsObject;
 
 final class MarkInstallUserEmailVerifiedAction
 {
+    use AsFake;
     use AsObject;
 
     public function handle(Authenticatable $user): void
